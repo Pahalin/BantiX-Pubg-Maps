08b3748689c78734228b1420d98b21309578eef8
Pahalin <pahalinqq@gmail.com>
2025-11-16 04:04:46 +0300

    первый коммит

---
+++ BantiX Pubg Maps 4/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace BX_PUBG_MAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Erangel = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            Paramo = new PictureBox();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            Deston = new PictureBox();
+            DestonLabel = new Label();
+            Rondo = new PictureBox();
+            Vikendi = new PictureBox();
+            Taego = new PictureBox();
+            pictureBox1 = new PictureBox();
+            notifyIcon1 = new NotifyIcon(components);
+            button7 = new Button();
+            button8 = new Button();
+            label1 = new Label();
+            CheckUpdates = new Button();
+            ((System.ComponentModel.ISupportInitialize)Erangel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Paramo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Deston).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Rondo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Vikendi).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Taego).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // Erangel
+            // 
+            Erangel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            Erangel.Image = (Image)resources.GetObject("Erangel.Image");
+            Erangel.Location = new Point(210, 1);
+            Erangel.Margin = new Padding(4, 3, 4, 3);
+            Erangel.Name = "Erangel";
+            Erangel.Size = new Size(1139, 1314);
+            Erangel.TabIndex = 3;
+            Erangel.TabStop = false;
+            Erangel.Visible = false;
+            Erangel.Click += Erangel_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 192, 255);
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(14, 91);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 51);
+            button1.TabIndex = 4;
+            button1.TabStop = false;
+            button1.Text = "Эрангель";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(192, 192, 255);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(14, 380);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 51);
+            button2.TabIndex = 5;
+            button2.TabStop = false;
+            button2.Text = "Парамо";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Paramo
+            // 
+            Paramo.Image = (Image)resources.GetObject("Paramo.Image");
+            Paramo.Location = new Point(210, 1);
+            Paramo.Margin = new Padding(4, 3, 4, 3);
+            Paramo.Name = "Paramo";
+            Paramo.Size = new Size(1177, 940);
+            Paramo.TabIndex = 6;
+            Paramo.TabStop = false;
+            Paramo.Visible = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(192, 192, 255);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.Location = new Point(14, 322);
+            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 51);
+            button3.TabIndex = 7;
+            button3.TabStop = false;
+            button3.Text = "Дестон";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(192, 192, 255);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button4.Location = new Point(14, 264);
+            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(170, 51);
+            button4.TabIndex = 8;
+            button4.TabStop = false;
+            button4.Text = "Рондо";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(192, 192, 255);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button5.Location = new Point(14, 149);
+            button5.Margin = new Padding(4, 3, 4, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(170, 51);
+            button5.TabIndex = 9;
+            button5.TabStop = false;
+            button5.Text = "Викенди";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(192, 192, 255);
+            button6.BackgroundImageLayout = ImageLayout.None;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Bookman Old Style", 15F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button6.Location = new Point(14, 207);
+            button6.Margin = new Padding(4, 3, 4, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(170, 51);
+            button6.TabIndex = 10;
+            button6.TabStop = false;
+            button6.Text = "Таего";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // Deston
+            // 
+            Deston.Image = (Image)resources.GetObject("Deston.Image");
+            Deston.Location = new Point(210, -2);
+            Deston.Margin = new Padding(4, 3, 4, 3);
+            Deston.Name = "Deston";
+            Deston.Size = new Size(1166, 1182);
+            Deston.TabIndex = 11;
+            Deston.TabStop = false;
+            Deston.Visible = false;
+            // 
+            // DestonLabel
+            // 
+            DestonLabel.AutoSize = true;
+            DestonLabel.BackColor = Color.DarkOrange;
+            DestonLabel.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            DestonLabel.Location = new Point(897, -2);
+            DestonLabel.Margin = new Padding(4, 0, 4, 0);
+            DestonLabel.Name = "DestonLabel";
+            DestonLabel.Size = new Size(410, 60);
+            DestonLabel.TabIndex = 12;
+            DestonLabel.Text = "🔴 Секретные Трейлеры \r\n█ Секретные Комнаты\r\n█ ⬆️ Секретные Комнаты на крыше или на последнем этаже высотки\r\n█ ⬇️ Подземные Секретные Комнаты\r\n";
+            DestonLabel.Visible = false;
+            // 
+            // Rondo
+            // 
+            Rondo.Image = (Image)resources.GetObject("Rondo.Image");
+            Rondo.Location = new Point(220, -33);
+            Rondo.Margin = new Padding(4, 3, 4, 3);
+            Rondo.Name = "Rondo";
+            Rondo.Size = new Size(1281, 1270);
+            Rondo.TabIndex = 13;
+            Rondo.TabStop = false;
+            Rondo.Visible = false;
+            Rondo.Click += Rondo_Click;
+            // 
+            // Vikendi
+            // 
+            Vikendi.Image = (Image)resources.GetObject("Vikendi.Image");
+            Vikendi.Location = new Point(210, -2);
+            Vikendi.Margin = new Padding(4, 3, 4, 3);
+            Vikendi.Name = "Vikendi";
+            Vikendi.Size = new Size(1166, 1160);
+            Vikendi.TabIndex = 14;
+            Vikendi.TabStop = false;
+            Vikendi.Visible = false;
+            // 
+            // Taego
+            // 
+            Taego.BorderStyle = BorderStyle.FixedSingle;
+            Taego.Image = (Image)resources.GetObject("Taego.Image");
+            Taego.Location = new Point(210, -2);
+            Taego.Margin = new Padding(4, 3, 4, 3);
+            Taego.Name = "Taego";
+            Taego.Size = new Size(1177, 1159);
+            Taego.TabIndex = 15;
+            Taego.TabStop = false;
+            Taego.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-7, -33);
+            pictureBox1.Margin = new Padding(4, 3, 4, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1508, 1244);
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(255, 192, 192);
+            button7.BackgroundImageLayout = ImageLayout.None;
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button7.Location = new Point(14, 792);
+            button7.Margin = new Padding(4, 3, 4, 3);
+            button7.Name = "button7";
+            button7.Size = new Size(170, 51);
+            button7.TabIndex = 17;
+            button7.TabStop = false;
+            button7.Text = "✘ Убрать все";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(255, 192, 192);
+            button8.BackgroundImageLayout = ImageLayout.None;
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.Font = new Font("Microsoft Sans Serif", 13F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button8.Location = new Point(14, 849);
+            button8.Margin = new Padding(4, 3, 4, 3);
+            button8.Name = "button8";
+            button8.Size = new Size(170, 51);
+            button8.TabIndex = 18;
+            button8.TabStop = false;
+            button8.Text = "↩ Закрыть";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(10, 10);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 19;
+            label1.Text = "4.0.2";
+            // 
+            // CheckUpdates
+            // 
+            CheckUpdates.BackColor = Color.FromArgb(255, 192, 192);
+            CheckUpdates.BackgroundImageLayout = ImageLayout.None;
+            CheckUpdates.FlatStyle = FlatStyle.Popup;
+            CheckUpdates.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            CheckUpdates.Location = new Point(14, 736);
+            CheckUpdates.Margin = new Padding(4, 3, 4, 3);
+            CheckUpdates.Name = "CheckUpdates";
+            CheckUpdates.Size = new Size(170, 48);
+            CheckUpdates.TabIndex = 20;
+            CheckUpdates.TabStop = false;
+            CheckUpdates.Text = "Проверить обноление";
+            CheckUpdates.UseVisualStyleBackColor = false;
+            CheckUpdates.Click += CheckUpdates_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1496, 1061);
+            Controls.Add(CheckUpdates);
+            Controls.Add(label1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(Taego);
+            Controls.Add(Vikendi);
+            Controls.Add(Rondo);
+            Controls.Add(DestonLabel);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(Paramo);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(Erangel);
+            Controls.Add(Deston);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BXPM | BantiX Pubg Maps ";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)Erangel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Paramo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Deston).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Rondo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Vikendi).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Taego).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Erangel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox Paramo;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox Deston;
+        private System.Windows.Forms.Label DestonLabel;
+        private System.Windows.Forms.PictureBox Rondo;
+        private System.Windows.Forms.PictureBox Vikendi;
+        private System.Windows.Forms.PictureBox Taego;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CheckUpdates;
+    }
+}
+
---
+++ BantiX Pubg Maps 4/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BX_PUBG_MAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Text = "PUBG Map Viewer";
+            notifyIcon1.Visible = true;
+
+            notifyIcon1.BalloonTipTitle = "Добро пожаловать!";
+            notifyIcon1.BalloonTipText = "Программа BantiX Pubg Maps успешно запущена.";
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+        private void tabPage1_Click(object sender, EventArgs e) { }
+        private void Erangel_Click(object sender, EventArgs e) { }
+        private void Rondo_Click(object sender, EventArgs e) { }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Erangel.Visible = true;
+            Paramo.Visible = false;
+            Deston.Visible = false;
+            DestonLabel.Visible = false;
+            Rondo.Visible = false;
+            Vikendi.Visible = false;
+            Taego.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Paramo.Visible = true;
+            Erangel.Visible = false;
+            Deston.Visible = false;
+            DestonLabel.Visible = false;
+            Rondo.Visible = false;
+            Vikendi.Visible = false;
+            Taego.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Deston.Visible = true;
+            Paramo.Visible = false;
+            Erangel.Visible = false;
+            DestonLabel.Visible = true;
+            Rondo.Visible = false;
+            Vikendi.Visible = false;
+            Taego.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Rondo.Visible = true;
+            Deston.Visible = false;
+            Paramo.Visible = false;
+            Erangel.Visible = false;
+            DestonLabel.Visible = false;
+            Vikendi.Visible = false;
+            Taego.Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Vikendi.Visible = true;
+            Rondo.Visible = false;
+            Deston.Visible = false;
+            Paramo.Visible = false;
+            Erangel.Visible = false;
+            DestonLabel.Visible = false;
+            Taego.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Taego.Visible = true;
+            Vikendi.Visible = false;
+            Rondo.Visible = false;
+            Deston.Visible = false;
+            Paramo.Visible = false;
+            Erangel.Visible = false;
+            DestonLabel.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Taego.Visible = false;
+            Vikendi.Visible = false;
+            Rondo.Visible = false;
+            Deston.Visible = false;
+            Paramo.Visible = false;
+            Erangel.Visible = false;
+            DestonLabel.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async Task CheckForUpdateAsync(string owner, string repo)
+        {
+            try
+            {
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("BX-PUBG-MAP-Updater");
+
+                string url = $"https://api.github.com/repos/{owner}/{repo}/releases/latest";
+                string json = await client.GetStringAsync(url);
+
+                using var doc = JsonDocument.Parse(json);
+                string tag = doc.RootElement.GetProperty("tag_name").GetString();
+                string latestVersion = tag.TrimStart('v');
+                string currentVersion = Application.ProductVersion;
+
+                if (latestVersion != currentVersion)
+                {
+                    if (doc.RootElement.TryGetProperty("assets", out JsonElement assets) && assets.GetArrayLength() > 0)
+                    {
+                        string downloadUrl = assets[0].GetProperty("browser_download_url").GetString();
+                        string newExePath = Path.Combine(Application.StartupPath, "BX-PUBG-MAP_new.exe");
+
+                        using var wc = new WebClient();
+                        wc.DownloadFile(downloadUrl, newExePath);
+
+                        notifyIcon1.BalloonTipTitle = "Обновление";
+                        notifyIcon1.BalloonTipText = $"Новая версия {latestVersion} загружена.";
+                        notifyIcon1.ShowBalloonTip(3000);
+
+                        string currentExePath = Application.ExecutablePath;
+                        string backupPath = Path.Combine(Application.StartupPath, "BX-PUBG-MAP_old.exe");
+
+                        try
+                        {
+                            File.Move(currentExePath, backupPath);
+                            File.Move(newExePath, currentExePath);
+
+                            System.Diagnostics.Process.Start(currentExePath);
+                            Application.Exit();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка при замене файла: {ex.Message}", "Обновление");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл обновления не найден в релизе.", "Обновление");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("У вас уже последняя версия.", "Обновление");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при проверке обновлений:\n{ex.Message}", "Обновление");
+            }
+        }
+
+        private async void CheckUpdates_Click(object sender, EventArgs e)
+        {
+            await CheckForUpdateAsync("Pahalin", "BantiX-Pubg-Maps");
+        }
+    }
+}
